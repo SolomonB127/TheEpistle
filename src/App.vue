60522121a4f73dc55151dbe3580bc0ab4c6fdfd6
SolomonB127 <solomonakinsulire@gmail.com>
2023-08-29 00:39:36 +0100

    Modified Subscribe button and textField

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,10 @@ import { RouterLink, RouterView } from 'vue-router';
 export default{
   data(){
     return{
-      Sbtn : true,
       isOpen: false,
+      email: '',
+      error: '',
+      isSubscribed: false,
     };
   },
   methods: {
@@ -14,97 +16,111 @@ export default{
     },
     closeNav(){
       this.isOpen = false;
-    }
-  }
+    },
+    subscribe() {
+      // Simulate a subscription by updating the isSubscribed state
+      if (this.email.trim() === '') {
+        this.error = 'Please enter a valid email.';
+        return;
+      }
+
+      // Simulate a successful subscription
+      this.isSubscribed = true;
+      this.email = ''; // Clear the input field after subscribing
+      this.error = '';
+    },
+  },
 }
 
 </script>
 
 <template>
-  <!-- Head/Nav Section -->
-  <header>
-      <nav>
-        <!-- Side nav -->
-        <div id="mainSidenav">
-          <span style="font-size: 30px; cursor: pointer" @click="openNav">&#9776;</span>
-          <!-- Sidebar -->
-          <div
-            id="mySidenav"
-            class="sidenav"
-            :style="{ width: isOpen ? '250px' : '0' }"
-          >
-            <h3>The Epistle</h3>
-            <a class="closebtn" @click="closeNav">&times;</a>
-            <RouterLink to="/">Home</RouterLink>
-            <RouterLink to="/about">About</RouterLink>
-            <RouterLink to="/entries">DiaryEntries</RouterLink>
-            <RouterLink to="/contact">Contact</RouterLink>
+    <!-- Head/Nav Section -->
+    <header>
+        <nav>
+          <!-- Side nav -->
+          <div id="mainSidenav">
+            <span style="font-size: 30px; cursor: pointer" @click="openNav">&#9776;</span>
+            <!-- Sidebar -->
+            <div
+              id="mySidenav"
+              class="sidenav"
+              :style="{ width: isOpen ? '250px' : '0' }"
+            >
+              <h3>The Epistle</h3>
+              <a class="closebtn" @click="closeNav">&times;</a>
+              <RouterLink to="/">Home</RouterLink>
+              <RouterLink to="/about">About</RouterLink>
+              <RouterLink to="/entries">DiaryEntries</RouterLink>
+              <RouterLink to="/contact">Contact</RouterLink>
+            </div>
           </div>
-        </div>
-        <h1>The Epistle</h1>
-        <ul>
-          <li>
-            <RouterLink to="/">Home</RouterLink>
-          </li>
-          <li>
-            <RouterLink to="/about">About</RouterLink>
-          </li>
-          <li>
-            <RouterLink to="/entries">DiaryEntries</RouterLink>
-          </li>
-          <li>
-            <RouterLink to="/contact">Contact</RouterLink>
-          </li>
-        </ul>
+          <h1>The Epistle</h1>
+          <ul>
+            <li>
+              <RouterLink to="/">Home</RouterLink>
+            </li>
+            <li>
+              <RouterLink to="/about">About</RouterLink>
+            </li>
+            <li>
+              <RouterLink to="/entries">DiaryEntries</RouterLink>
+            </li>
+            <li>
+              <RouterLink to="/contact">Contact</RouterLink>
+            </li>
+          </ul>
 
-        <ul>
-          <li>
-            <img src="pictures\icons8-dark-24.png" alt="" id="dark">
-          </li>
-          <li>
-            <img src="pictures\icons8-search-150.png" alt=""  width="20" @click="Sbtn = !Sbtn" id="S-btn">
-            
-          </li>
-        </ul>
-      </nav> 
-  </header>
-  <span>
-    <input type="search" name="" id="search" placeholder="Search Here..." :class="{button : Sbtn}">
-  </span>
+          <!-- <ul>
+            <li>
+              <img alt="" id="dark" v-if="isDarkTheme" src="pictures\icons8-sun-24.png" @click="toggleTheme">
+              <img
+                v-else
+                src="pictures\icons8-dark-24.png"
+                alt=""
+                id="dark"
+                @click="toggleTheme"
+              />
+            </li>
+          </ul> -->
+        </nav> 
+    </header>
 
-
-  <RouterView />
-
-
+      <RouterView />
+   
   <!-- Footer -->
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col">
-        <section id="footer">
-          <h2>
-            Stay In Touch.
-          </h2>
-          <div class="footer">
-            <p>Subscribe to stay notified.</p>
-            <p><input type="email" name="subscribe" class="sub-box" placeholder="Enter e-mail..." autofocus="off"></p>
-            <button id="sub-btn" class="sub-btn">Subscribe</button>
-          </div>
-          <div id="social">
-            <div>
-              <a href="https://www.facebook.com"><img src="pictures\icons8-facebook-120.png" alt="facebook" width="50"></a>
+  <footer>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col">
+          <section id="footer">
+            <h2>
+              Stay In Touch.
+            </h2>
+            <div class="footer">
+              <p>Subscribe to stay notified.</p>
+              <p><input type="email" v-model="email" name="subscribe" class="sub-box" placeholder="Enter e-mail..." autofocus="off"></p>
+              <button id="sub-btn" class="sub-btn" @click="subscribe">Subscribe</button>
+              <p v-if="error" class="error-msg">{{ error }}</p>
+              <p v-if="isSubscribed" class="success-msg">Thank you for subscribing!</p>
             </div>
-            <div>
-              <a href="https://www.pinterest.com"><img src="pictures\icons8-pinterest-120.png" alt="pinterest" width="50"></a>
+            <div id="social">
+              <div>
+                <RouterLink to="https://www.facebook.com"><img src="pictures\icons8-facebook-120.png" alt="facebook" width="50"></RouterLink>
+              </div>
+              <div>
+                <RouterLink to="https://www.pinterest.com"><img src="pictures\icons8-pinterest-120.png" alt="pinterest" width="50"></RouterLink>
+              </div>
+              <div>
+                <RouterLink to="https://www.instagram.com"><img src="pictures\icons8-instagram-120.png" alt="instagram" width="50"></RouterLink>
+              </div>
             </div>
-            <div>
-              <a href="https://www.instagram.com"><img src="pictures\icons8-instagram-120.png" alt="instagram" width="50"></a>
-            </div>
-          </div>
-          <section id="rights">&copy;copyright 2023.</section>
-        </section>
+            <section id="rights">&copy;copyright 2023.</section>
+          </section>
+        </div>
       </div>
     </div>
-  </div>
+  </footer>
 </template>
 
 <style scoped>
@@ -143,12 +159,6 @@ export default{
  #search{
  width: 100%;
  height: 40px;
- }
- #S-btn{
-  cursor: pointer;
- }
- .button{
-  display: none;
  }
  #mainSidenav{
   display: none;
@@ -283,6 +293,13 @@ export default{
     margin-top: 30px;
     font-size: 1.25rem;
     font-weight: bolder;
+}
+/* Error handling */
+.error-msg {
+  color: red;
+}
+.success-msg {
+  color: green;
 }
 /* Subscription */
 @media all and (200px <= width <= 1024px) {
